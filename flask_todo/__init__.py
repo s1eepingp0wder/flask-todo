3671d811003693c922d4c465abe62b3a9117355f
Danny Liehr <dannyisyelling@gmail.com>
2019-03-24 23:46:59 -0400

    Created list

--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -1,4 +1,10 @@
 # http://flask.pocoo.org/docs/1.0/quickstart/#routing
+
+# Built in Python stuff
+import datetime
+import time
+
+# Imported packages
 from flask import Flask, request, make_response, render_template
 
 def page_not_found(e):
@@ -18,53 +24,17 @@ def create_app(test_config=None):
 		app.config.from_mapping(test_config)
 
 	# Let's make some lists.
-
-	request_methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
-	@app.route('/', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+	ts= time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	# print(st)
+	todo_list=[
+		['Item 1', 'Description 1', st, True],
+		['Item 2', 'Description 2', st, False],
+		['Item 3', 'Description 3', st, False],
+	]
+	@app.route('/', methods=['GET', 'POST'])
 	def index():
-		# return 'This is a boilerplate.'
-		return render_template('index.html', len= len(request_methods), request_methods= request_methods, method= request.method)
+		return render_template('index.html', len= len(todo_list), todos= todo_list, st= st)
 
-	@app.route('/hello')
-	def hello():
-		for key, value in request.args.items():
-			print(f"{key}: {value}")
-		name = request.args.get('name', 'World')
-		# return f"Hello {name}!"
-		return render_template('hello.html', name=name)
-
-	@app.route('/number/<int:n>')
-	def number_route(n):
-		return f"Number: {n}"
-
-	@app.route('/method', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
-	def method_route():
-		return f"HTTP Method: {request.method}"
-
-	@app.route('/status')
-	def status_route():
-		code = request.args.get('c', 200)
-		response = make_response('', code)
-
-	@app.route('/calculate', methods=['GET', 'POST'])
-	def calculate_page():
-		result= None
-		if request.method=="GET":
-			return render_template('calculate.html', result= result)
-		elif request.method=="POST":
-			x= float(request.form['x'])
-			y= float(request.form['y'])
-			action= request.form['action']
-			if action=="Add":
-				result= x + y
-			elif action=="Subtract":
-				result= x-y
-			elif action=="Multiply":
-				result = x * y
-			elif action=="Divide":
-				result = x/y
-			return render_template('calculate.html', result= result, x=x, y=y)
-
-		return response
 	# End App
 	return app
