6437f11d5fcbfc8b9009d252a7029413fe848f87
Danny Liehr <dannyisyelling@gmail.com>
2019-03-28 16:17:37 -0400

    Nightly commit. Adding login requirements.

--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -5,9 +5,23 @@ import datetime
 import time
 import random
 import psycopg2
+import functools
+
 
 # Imported packages
-from flask import Flask, request, make_response, render_template, flash, redirect, url_for
+from flask import Flask, request, make_response, render_template, flash, redirect, url_for, g, session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def login_required(view):
+    """View decorator that redirects anonymous users to the login page."""
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
 
 def page_not_found(e):
 	return render_template('404.html')
@@ -35,22 +49,31 @@ def create_app(test_config=None):
 	# Thanks, mate!
 
 	# Let's do some trickery: Look for this table! If you can't find it, make it.
+	# Now try users.
+	try:
+		# Can we get in?
+		cur.execute("SELECT * FROM users;")
+	except:
+		# No. It doesn't exist. Let's make it.
+		cur.execute("CREATE TABLE users(id SERIAL PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL);")
+		print("Created table \"tasks\".")
+
 	try:
 		# Can we get in?
 		cur.execute("SELECT * FROM tasks;")
 	except:
 		# No. It doesn't exist. Let's make it.
-		cur.execute("CREATE TABLE tasks(name TEXT, description TEXT, time TIMESTAMP, completed BOOLEAN DEFAULT false, task_id SERIAL PRIMARY KEY)")
-		cur.execute("INSERT INTO tasks (name, description, time, completed) VALUES (%s, %s, %s, %s)", ("Hello, World!", "Your first task is to read this task!", st, False))
+		cur.execute("CREATE TABLE tasks(name TEXT, description TEXT, time TIMESTAMP, completed BOOLEAN DEFAULT false, task_id SERIAL PRIMARY KEY, author_id SERIAL NOT NULL, FOREIGN KEY (author_id) REFERENCES users (id))")
 		print("Created table \"tasks\".")
 
-
 	sql_query = 'SELECT name, description, time, completed, task_id FROM tasks ORDER BY time DESC'
+
 	cur.execute(sql_query)
 	todo_list = cur.fetchall()
 	# print(todo_list)
 
 	@app.route('/', methods=['GET', 'POST', 'DELETE'])
+	@login_required
 	def index():
 		cur.execute(sql_query)
 		todo_list = cur.fetchall()
@@ -78,6 +101,7 @@ def create_app(test_config=None):
 		return task
 
 	@app.route('/<int:id>/update', methods=['GET', 'POST'])
+	@login_required
 	def update(id):
 		print(f"Updating Task ID {id}")
 		get_task(id)
@@ -86,12 +110,43 @@ def create_app(test_config=None):
 		return redirect(url_for('index'))
 
 	@app.route('/<int:id>/delete', methods=('POST','GET'))
+	@login_required
 	def delete(id):
 		get_task(id)
 		cur.execute('DELETE FROM tasks WHERE task_id= %s',[id])
 		conn.commit()
 		return redirect(url_for('index'))
 
+	@app.route('/login', methods=['GET', 'POST'])
+	def log_in():
+		if request.method == "POST":
+			username = request.form['username']
+			password = request.form['password'] # HASH THIS LATER!!
+
+			cur.execute('SELECT * FROM users WHERE username= %s', (username,))
+			user= cur.fetchone()
+			if user is None:
+				flash("That name doesn't exist yet! You can sign up!")
+			else:
+				session.clear()
+				session['user_id'] = user[0]
+				return redirect(url_for('index'))
+		return render_template('login.html')
+
+	@app.route('/register', methods=['GET', 'POST'])
+	def register():
+		if request.method == "POST":
+			username = request.form['username']
+			password = request.form['password']
+			cur.execute('SELECT id FROM users WHERE username= %s', (username,))
+			user = cur.fetchone()
+			if user is not None:
+				flash("That username already exists. Did you mean to log in?")
+			else :
+				cur.execute('INSERT INTO users (username, password) VALUES (?,?)',(username, generate_password_hash(password)))
+				conn.commit()
+				return redirect(url_for('log_in'))
+		return render_template('register.html')
 
 	# End App
 	return app
